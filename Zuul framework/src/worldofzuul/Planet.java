cef463e2bcc6a1e10da4874d5629fcaaffcf647b
Kasper Christensen <Kasper@Kaspers-MacBook-Air.local>
2016-10-12 15:08:42 +0200

    Rettet version efter at have taget imod NetBeans' råd. Fx er attributterne nu 'final'.

--- Zuul framework/src/worldofzuul/Planet.java
+++ Zuul framework/src/worldofzuul/Planet.java
@@ -1,50 +1,53 @@
 
-
 public class Planet {
 
-	//Initializing variables
-	private String description;
-	private String name;
-	private int xCoor;
-	private int yCoor;
-	private int numOfMoons;
-		
+    //Initializing variables
+    private final String description;
+    private final String name;
+    private final int xCoor;
+    private final int yCoor;
+    private final int numOfMoons;
+			
 
-	//Creating list for planets
-	public static ArrayList<Planet> planets;
-	planets = new ArrayList<Planet>;
+    /** Creating list of planets.
+     * This list will contain all created instanses of the class Planet
+     * as per the constructor.
+     */
+    public static ArrayList<Planet> planets = new ArrayList<>();
+    
+    //Constructor
+    public Planet(String name, String description, int xCoor, int yCoor, 
+                  int numOfMoons, int NPCid) {	
+            
+	this.name = name;
+	this.description = description;
+	this.xCoor = xCoor;
+	this.yCoor = yCoor;
+	this.numOfMoons = numOfMoons;
+	planets.add(this);	//Adding the created object to the list of planets
+
+	NPC npc = new NPC(NPCid);
+    }
 
 
-	//Constructor
-	public Planet(String name, String description, int xCoor, int yCoor, int numOfMoons) {		
-		this.name = name;
-		this.description = description;
-		this.xCoor = xCoor;
-		this.yCoor = yCoor;
-		this.numOfMoons = numOfMoons;
-		planets.add(this);	//Adding the created object to the list of planets
+    //Getters
+    public int getNumOfMoons() {
+    	return numOfMoons;
+    }
 
-	}
+    public String getName() {
+	return name;
+    }
 
+    public String getDescription() {
+	return description;
+    } 
 
-	//Getters
-	public int getNumOfMoons() {
-		return moons.size();
-	}
+    public int getXCoor() {
+	return xCoor;
+    }
 
-	public String getName() {
-		return name;
-	}
-
-	public String getDescription() {
-		return description;
-	} 
-
-	public int getXCoor() {
-		return xCoor;
-	}
-
-	public int getYCoor() {
-		return yCoor;
-	}
+    public int getYCoor() {
+	return yCoor;
+    }
 }
