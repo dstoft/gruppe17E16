37efcd07afb1c9c28cdd5b27138d2c18316bce80
EmilHarder <mail@emilharder.dk>
2016-10-24 14:19:45 +0200

    Added setMaxItems, setMaxWeight and remItems. First two works. Third one, not quite.

--- Zuul framework/src/worldofzuul/Inventory.java
+++ Zuul framework/src/worldofzuul/Inventory.java
@@ -1,7 +1,6 @@
 package worldofzuul; // HUSK
 
 import java.util.ArrayList; //Imports the utility for the Arraylist
-import java.util.Iterator;
 
 /**
  * Inventory - thats all (skriv mere)
@@ -11,21 +10,22 @@ import java.util.Iterator;
 public class Inventory { // Initializing the class Inventory
 
     /**
-     * Following are the two parameters for the inventory that can be set when
-     * constructed. But if the class is constructed without them being set, it
-     * will fall back to these predefined values:
-     *
-     * maxAllowedItems with default 3 maxAllowedWeight with default 12
-     *
-     */
-    private int maxAllowedItems = 3;
-    private int maxAllowedWeight = 12;
-
-    /**
      * Next is the declaration of the ArrayList witch will be the structure for
      * the inventory. (Adding/removing items etc)
      */
     private final ArrayList<Items> inventoryList;
+
+    /**
+     * Following are the two parameters for the inventory that can be set when
+     * constructed. But if the class is constructed without them being set, it
+     * will fall back to these predefined values:
+     *
+     * maxAllowedItems with default 3
+     * maxAllowedWeight with default 12
+     *
+     */
+    private int maxAllowedItems = 3;
+    private int maxAllowedWeight = 12;
 
     /**
      * The inventory will have some values about how many items there are in it,
@@ -41,12 +41,12 @@ public class Inventory { // Initializing the class Inventory
      * weight the player is allowed to carry and the maximum amount of items
      * allowed to carry. If no values are given, the defaults will be used.
      *
-     * @param maxWeight set max allowed weight in integers, default is 12.
      * @param maxItems set max allowed amount of items in integers default is 3.
+     * @param maxWeight set max allowed weight in integers, default is 12.
      *
      */
     // The version of Inventory with parameters given.
-    public Inventory(int maxWeight, int maxItems) {
+    public Inventory(int maxItems, int maxWeight) {
 
         this.maxAllowedItems = maxItems;
         this.maxAllowedWeight = maxWeight;
@@ -60,6 +60,25 @@ public class Inventory { // Initializing the class Inventory
     }
 
     /**
+     * getMaxItems returns the maximum amount of items allowed to carry.
+     *
+     * @return maximum amount of items allowed in integers.
+     */
+    public int getMaxItems() {
+
+        return maxAllowedItems;
+    }
+
+    /**
+     * setMaxItems sets the max amount of items allowed to carry.
+     * 
+     * @param x int for setting maxAllowedItems
+     */
+    public void setMaxItems(int x){
+        this.maxAllowedItems = x;
+    }
+
+    /**
      * getMaxWeight returns the maximum cumulated weight allowed to carry.
      *
      * @return maximum cumulated weight allowed in integers.
@@ -70,19 +89,18 @@ public class Inventory { // Initializing the class Inventory
     }
 
     /**
-     * getMaxWeight returns the maximum amount of items allowed to carry.
-     *
-     * @return maximum amount of items allowed in integers.
+     * setMaxWeight set the max cumulated weight allowed to carry.
+     * 
+     * @param x int for setting maxAllowedWeight
      */
-    public int getMaxItems() {
-
-        return maxAllowedItems;
+    public void setMaxWeight(int x){
+        this.maxAllowedWeight = x;
     }
 
     /**
      * This method adds a item, of type Item, to the inventory.
      *
-     * TODO: There needs a destination too..
+     * TODO: There needs a destination too.. and a return og the ID number
      *
      * @param weight Set the weight of the item (int).
      * @param name Set the name of the item (String).
@@ -97,6 +115,10 @@ public class Inventory { // Initializing the class Inventory
 
         inventoryList.add(item);
 
+    }
+    
+    public void remItem(int x){
+        inventoryList.remove(x);
     }
 
     /**
