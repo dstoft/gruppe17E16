550ce9d9a1569bd0da48fa69ffcaca006cc3e7a1
emil <emil@emils-desktop>
2016-10-26 00:13:38 +0200

    Out-commented if-else statement in getPapers and simply returning the var.

--- Zuul framework/src/worldofzuul/Items.java
+++ Zuul framework/src/worldofzuul/Items.java
@@ -60,12 +60,12 @@ public class Items {
          return name;
     }
 
-    public int xCoor() {        //Method for returning  destination x coordinate
+    public int getXCoor() {        //Method for returning  destination x coordinate
 
          return xCoor;
     }
     
-     public int yCoor() {        //Method for returning  destination y coordinate
+     public int getYCoor() {        //Method for returning  destination y coordinate
 
          return yCoor;
     }
@@ -81,14 +81,16 @@ public class Items {
     }
 
     public boolean getPapers() {         //Method to check if the user have papers on the item.
-         if (papers == false) {                
-            System.out.println("You don't have papers on that pakage!");
-                return false;
+        /*if (papers == false) {
+        System.out.println("You don't have papers on that pakage!");
+        return false;
         }
-                                                        //If the user have the paper, returns true, if not, returns false.
-         else
-            System.out.println("Your paperwork is just fine!");
-              return true;
-        }
+        //If the user have the paper, returns true, if not, returns false.
+        else
+        System.out.println("Your paperwork is just fine!");
+        return true;
+        }*/
+        return papers;
+    }
 
 }
