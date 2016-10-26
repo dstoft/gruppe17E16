fbd4b22812aefcc4226083edb641e8be08475fb5
EmilHarder <mail@emilharder.dk>
2016-10-26 15:56:42 +0200

    Begun to implement UUID into Items and Inventory. No test-run.

--- Zuul framework/src/worldofzuul/Items.java
+++ Zuul framework/src/worldofzuul/Items.java
@@ -1,5 +1,8 @@
 package worldofzuul; // HUSK
 
+import java.util.UUID;
+
+
 /**
  *
  * @author Thilos
@@ -16,35 +19,29 @@ public class Items {
 */
     
 //Initializing variables
-    private int id; //Every item have an ID.
-    private int weight; // The weight of the item.
-    private String name; // The name of the item
-    private String description; // The description of the item.
-    private String destination; // The destination of the item - paused for now..
+    private UUID id; //Every item have an ID
+    private int weight; // The weight of the item
+    private String description; // The description of the item
     private int xCoor;  // The x coordinate for the destination 
-    private int yCoor;  // The y coordinate for the destination 
-    private boolean papers; // Does the user have papers on the item
+    private int yCoor;  // The y coordinate for the destination
+    //private boolean papers; // Does the user have papers on the item
 
 //Initializing Constructor
 
     /**
      *
-     * @param id
-     * @param name
      * @param weight
      * @param desciption
      * @param xCoor
      * @param yCoor
-     * @param papers
      */
-    public Items(int id, String name, int weight, String desciption, int xCoor, int yCoor, boolean papers) {
-        this.id = id;
+    public Items(int weight, String desciption, int xCoor, int yCoor) {
+        this.id = UUID.randomUUID();
         this.weight = weight;
-        this.name = name;
         this.description = desciption;
         this.xCoor = xCoor;
         this.yCoor = yCoor;
-        this.papers = papers;
+        //this.papers = papers;
         
     }
 
@@ -53,11 +50,6 @@ public class Items {
     public String getDescription() {        //Method for getting the description of the item.
 
          return description;
-    }
-    
-    public String getName() {        //Method for getting the description of the item.
-
-         return name;
     }
 
     public int getXCoor() {        //Method for returning  destination x coordinate
@@ -75,22 +67,13 @@ public class Items {
          return weight;
     }
     
-    public int getId() {        // //Method for getting the ID of the item.
+    public UUID getId() {        // //Method for getting the ID of the item.
 
          return id;
     }
 
-    public boolean getPapers() {         //Method to check if the user have papers on the item.
-        /*if (papers == false) {
-        System.out.println("You don't have papers on that pakage!");
-        return false;
-        }
-        //If the user have the paper, returns true, if not, returns false.
-        else
-        System.out.println("Your paperwork is just fine!");
-        return true;
-        }*/
-        return papers;
-    }
+    /*public boolean getPapers() {         //Method to check if the user have papers on the item.
+    return papers;
+    }*/
 
 }
