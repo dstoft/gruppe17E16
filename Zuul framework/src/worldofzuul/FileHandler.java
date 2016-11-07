8ed0c92e94ac6d766bd7e7dae0d55d8135a9b250
Øllebrød 3 <MER@llebrd3.local>
2016-11-07 10:56:53 +0100

    Revert "Revert "Added JSON to FileHandler class""
    This reverts commit df48bf3e704159f24f612c980ba449780f3b867c.

--- Zuul framework/src/worldofzuul/FileHandler.java
+++ Zuul framework/src/worldofzuul/FileHandler.java
@@ -1,12 +1,15 @@
 package worldofzuul;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import org.codehaus.jackson.map.ObjectMapper;
 
 public class FileHandler{
 
+    //Method for txt file
     public List<String> getText(int id) {
         List<String> text = null;
         try {
@@ -16,5 +19,12 @@ public class FileHandler{
             return null;
         }
         return text;
+    }
+    
+    
+    ObjectMapper mapper = new ObjectMapper();
+    //Method for JSON
+    public <T> T getJSON(String fileSource, Class<T> classType) throws IOException {
+        return mapper.readValue(new File(fileSource), classType);
     }
 }
