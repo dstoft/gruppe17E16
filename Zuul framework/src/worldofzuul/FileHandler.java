6750c4827b09d445413df12e99b14f6f8e254d5d
DANIEL-ACER\DanielToft <Dstoft@hotmail.com>
2016-11-09 12:02:24 +0100

    Added JSON method to filehander, along with a method to check whether a file exists or not. Next up: Changing Moons into being of the type Planets

--- Zuul framework/src/worldofzuul/FileHandler.java
+++ Zuul framework/src/worldofzuul/FileHandler.java
@@ -1,9 +1,11 @@
 package worldofzuul;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import org.codehaus.jackson.map.ObjectMapper;
 
 public class FileHandler{
 
@@ -17,4 +19,24 @@ public class FileHandler{
         }
         return text;
     }
+    
+    
+
+    //Method for JSON
+    public <T> T getJSON(String fileSource, Class<T> classType) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(new File(fileSource), classType);
+        } catch(IOException e) {
+            return null;
+        }
+    }
+    
+    public boolean doesFileExist(String fileSource) {
+        File file = new File(fileSource);
+        // .exists() will return true, if the fileSource is a directory (a folder), 
+        //but we want to check whether a file exists, so we have to make sure that the fileSource is not a directory
+        return (file.exists() && !file.isDirectory());
+    }
+
 }
