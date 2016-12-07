e2936046ed1c536514450d8241e922c6c3d84574
Daniel Anton Jørgensen <D.kogsbolle@hotmail.com>
2016-12-07 15:36:34 +0100

    Now with a thanks for playing, problem is that it's not implemented because it can not make the sound before the program quit. a little comments as well.

--- Zuul framework/src/worldofzuul/AudioPlayer.java
+++ Zuul framework/src/worldofzuul/AudioPlayer.java
@@ -5,7 +5,7 @@
  */
 package worldofzuul;
 
- import java.io.File;
+import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -13,10 +13,12 @@ import javax.sound.sampled.Clip;
 /**
  *
  * @author Thilos
+ * 
+ * This class is an AudioPlayer, it's has a lot of methods to play sounds. 
+ * The playMusic is a method for playing music the entire game
+ * while the others is for specific events.
  */
-
-public class AudioPlayer 
-{
+public class AudioPlayer {
 
     public void playMusic() throws Exception {
 
@@ -25,25 +27,30 @@ public class AudioPlayer
         clip.open(inputStream);
         clip.loop(Clip.LOOP_CONTINUOUSLY);
         Thread.sleep(10000); // looping as long as this thread is alive
- 
+
     }
-    
+
     public void playFly() throws Exception {
 
-   AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("data/music/fly.wav"));
+        AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("data/music/fly.wav"));
         Clip clip = AudioSystem.getClip();
         clip.open(inputStream);
         clip.start();
     }
-    
-    
+
     public void playWarp() throws Exception {
 
-   AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("data/music/Warp.wav"));
+        AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("data/music/Warp.wav"));
+        Clip clip = AudioSystem.getClip();
+        clip.open(inputStream);
+        clip.start();
+    }
+
+    public void playThanks() throws Exception {
+
+        AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("data/music/Thanks.wav"));
         Clip clip = AudioSystem.getClip();
         clip.open(inputStream);
         clip.start();
     }
 }
-    
-    
